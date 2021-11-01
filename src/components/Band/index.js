00f2c352a1e1a55a8c8520ab2a3ad3c38bce6d7c
Ravindra <ravindra.kk@doodleblue.com>
2021-11-01 15:40:15 +0530

    added the band

--- src/components/Band/index.js
+++ src/components/Band/index.js
@@ -3,6 +3,8 @@ import RowLayout from "../Layout/RowLayout";
 import BandMember from "../../assets/images/bandmember.jpg"
 import './Band.css'
 import NormalImg from "../common/NormalImg";
+import NormalRow from "../common/NormalRow";
+import NormalCol from "../common/NormalCol";
 
 const bandImg = [{
   imgUrl: BandMember,
@@ -19,15 +21,15 @@ const Band = () => {
   return (
     <RowLayout>
       <div
-        className="w3-container w3-content w3-center w3-padding-64"
+        className="band-container band-content band-center band-padding-64"
         id="band"
         style={{ border: "1px solid red"}}
       >
-        <h2 className="w3-wide">THE BAND</h2>
-        <p className="w3-opacity">
+        <h2 className="band-wide">THE BAND</h2>
+        <p className="band-opacity">
           <i>We love music</i>
         </p>
-        <p className="w3-justify">
+        <p className="band-justify">
           We have created a fictional band website. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
@@ -40,14 +42,19 @@ const Band = () => {
           nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat.
         </p>
-        <div className="w3-row w3-padding-32">
+        <div className="band-row band-padding-32">
+        <NormalRow>       
           {
             bandImg.map((data) => 
-            <div className="w3-third">
-              <p>Name</p>
-              <NormalImg alt={data.alt} src={data.imgUrl} classNames={"w3-round w3-margin-bottom"}/>
-            </div>)
+            <div className="band-third">
+              <NormalCol> 
+                <p>Name</p>              
+                <NormalImg alt={data.alt} src={data.imgUrl} classNames={"band-round band-margin-bottom w-60"}/>
+              </NormalCol>
+            </div>
+            )
           }
+          </NormalRow>
           </div>
         </div>
     </RowLayout>
