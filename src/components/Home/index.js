4d06eeffb4a5cc3b0270d2934013ccf29602b39a
Ravindra <ravindra.kk@doodleblue.com>
2021-11-01 13:17:42 +0530

    tour container is added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,11 +4,13 @@ import Image from '../Image'
 import Map from '../../assets/images/map.jpg'
 import Contact from '../Contact'
 import Tour from '../TourModal'
+import Band from '../Band'
 
 
 const Home = () => {
   return (
     <> 
+      <Band />
       <Tour /> 
       <Contact /> 
       <Image src={Map} alt="map" classNames={"grayscale-min"}/>
