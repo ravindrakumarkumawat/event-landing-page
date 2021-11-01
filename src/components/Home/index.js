1252937c09eba3a5af76f6d53bd242cebe036038
Ravindra <ravindra.kk@doodleblue.com>
2021-11-01 12:58:46 +0530

    data is added for contact

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Footer from '../Footer'
 import Image from '../Image'
 import Map from '../../assets/images/map.jpg'
-import RowLayout from '../Layout/RowLayout'
+import Contact from '../Contact'
+
 
 const Home = () => {
   return (
-    <>   
-          <Image src={Map} alt="map" classNames={"grayscale-min"}/>
-        <Footer />
+    <>  
+      <Contact /> 
+      <Image src={Map} alt="map" classNames={"grayscale-min"}/>
+      <Footer />
     </>
   )
 }
