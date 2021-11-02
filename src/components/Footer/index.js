39b47d7cb5410566caf7729ea679a2269f619ad6
Ravindra <ravindra.kk@doodleblue.com>
2021-11-02 10:54:02 +0530

    added the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import { AmazonOutlined, FacebookOutlined, GoogleOutlined, InstagramOutlined, LinkedinOutlined, TwitterOutlined } from '@ant-design/icons';
-import RowLayout from '../Layout/RowLayout';
+import RowLayout from '../Layout/RowLayout'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <RowLayout>
-      <div className="footer">
-        <div className="icons-list">
-          <FacebookOutlined />
-          <InstagramOutlined />
-          <GoogleOutlined />  
-          <TwitterOutlined />
-          <AmazonOutlined />
-          <LinkedinOutlined />
+    <div className="bg-grey">
+      <RowLayout>
+        <div className="footer">
+          <div className="icons-list">
+            <FacebookOutlined />
+            <InstagramOutlined />
+            <GoogleOutlined />  
+            <TwitterOutlined />
+            <AmazonOutlined />
+            <LinkedinOutlined />
+          </div>
+          <div>
+            <p>Powered by <a href="#" target="_blank">w3.css</a></p>
+          </div>
         </div>
-        <div>
-          <p>Powered by <a href="#" target="_blank">w3.css</a></p>
-        </div>
-      </div>
-    </RowLayout>
+      </RowLayout>
+    </div>
   )
 }
 
