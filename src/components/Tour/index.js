d3e6c15eafb9dddae330dc6d2d805bf907a05488
Ravindra <ravindra.kk@doodleblue.com>
2021-11-02 08:26:46 +0530

    list is completed

--- src/components/Tour/index.js
+++ src/components/Tour/index.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import RowLayout from '../Layout/RowLayout'
+import './Tour.css'
 
 const Tour = () => {
   return (
     <RowLayout>
     <div className="black" id="tour">
-    <div className="container content padding-64" style={{ maxWidth:"800px"}}>
+    <div className="container content padding-64">
       <h2 className="wide center">TOUR DATES</h2>
       <p className="opacity center"><i>Remember to book your tickets!</i></p><br />
 
-      <ul className="ul border white text-grey">
-        <li className="padding">September <span className="tag red margin-left">Sold out</span></li>
-        <li className="padding">October <span className="tag red margin-left">Sold out</span></li>
+      <ul className="ul border bg-white text-grey">
+        <li className="padding">September <span className="tag bg-red margin-left">Sold out</span></li>
+        <li className="padding">October <span className="tag bg-red margin-left">Sold out</span></li>
         <li className="padding">November <span className="badge right margin-right">3</span></li>
       </ul>
 
-      <div className="row-padding padding-32" style={{margin:"0 -16px"}}>
+      <div className="row-padding padding-32">
         <div className="third margin-bottom">
-          <img src="/w3images/newyork.jpg" alt="New York" style={{width:"100%"}} className="hover-opacity" />
+          <img src="/w3images/newyork.jpg" alt="New York" className="hover-opacity" />
           <div className="container white">
             <p><b>New York</b></p>
             <p className="opacity">Fri 27 Nov 2016</p>
@@ -26,7 +27,7 @@ const Tour = () => {
           </div>
         </div>
         <div className="third margin-bottom">
-          <img src="/w3images/paris.jpg" alt="Paris" style={{width:"100%"}} className="hover-opacity" />
+          <img src="/w3images/paris.jpg" alt="Paris" className="hover-opacity" />
           <div className="container white">
             <p><b>Paris</b></p>
             <p className="opacity">Sat 28 Nov 2016</p>
@@ -35,7 +36,7 @@ const Tour = () => {
           </div>
         </div>
         <div className="third margin-bottom">
-          <img src="/w3images/sanfran.jpg" alt="San Francisco" style={{width:"100%"}} className="hover-opacity" />
+          <img src="/w3images/sanfran.jpg" alt="San Francisco" className="hover-opacity" />
           <div className="container white">
             <p><b>San Francisco</b></p>
             <p className="opacity">Sun 29 Nov 2016</p>
